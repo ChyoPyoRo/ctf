e65b1602b69cdc3be9f7f1e0543833d0de8cc4e8
dlxla4820 <gorhfqhk@gmail.com>
2023-09-25 12:39:45 +0900

    login edit

--- src/main/java/kimdaehan/ctf/controller/AdminController.java
+++ src/main/java/kimdaehan/ctf/controller/AdminController.java
@@ -1,6 +1,8 @@
 package kimdaehan.ctf.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kimdaehan.ctf.auth.AuthenticationFacade;
+import kimdaehan.ctf.entity.User;
 import kimdaehan.ctf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,8 +18,15 @@ public class AdminController extends BaseController{
     }
 
     @GetMapping({"/admin_main"})
-    public ModelAndView adminMain(){
+    public ModelAndView adminMain(HttpServletRequest request){
+        User user = getUser();
         ModelAndView mv = new ModelAndView();
+        if(user.getType() != User.Type.ADMIN){
+            logger.error("Not Admin access this page -> user : {}, IP : {}", user.getUserId(), request.getRemoteAddr());
+            mv.setViewName("/404");
+            return mv;
+        }
+
         mv.setViewName("/admin/admin_main");
         return mv;
     }
