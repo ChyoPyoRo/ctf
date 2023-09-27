d7968915d20ea18caf0f30334d89dc5c760f313b
ChyoPyoRo <allen15590@naver.com>
2023-09-27 18:34:57 +0900

    준비중

---
+++ src/main/java/kimdaehan/ctf/entity/quiz.java
@@ -0,0 +1,15 @@
+package kimdaehan.ctf.entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class quiz {
+
+}
