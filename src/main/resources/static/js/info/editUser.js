f0d78fe6d5711048e6ddb91bda08add2b8cf51e2
dlxla4820 <gorhfqhk@gmail.com>
2023-10-14 18:19:55 +0900

    myinfo edit

---
+++ src/main/resources/static/js/info/editUser.js
@@ -0,0 +1,51 @@
+
+
+function editUser(){
+    const id = document.querySelector("input[id=id]").value;
+    const password = document.querySelector("input[id=password]").value;
+    const name = document.querySelector("input[id=name]").value;
+    const affiliation = document.querySelector("select[id=affiliation]").value;
+    const nickName = document.querySelector("input[id=nickName]").value;
+    const body = {
+        userId: id,
+        password: password,
+        name: name,
+        nickName : nickName,
+        affiliation : affiliation
+    };
+
+    const url = "/editUser";
+    const options = {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    };
+
+
+    if(isEmpty(removeSpace(id))){
+        alert("아이디가 공백입니다.");
+    }
+    else {
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data === 'OK') {
+                    window.location.href = "/logout";
+                    alert("정상적으로 변경 되었습니다.");
+                } else{
+                    alert("회원 가입 실패");
+                }
+            });
+    }
+}
+
+function isEmpty(value){
+    return value.length === 0;
+}
+function removeSpace(value) {
+    return value.replace(/\s/g,"");
+}
