e8092d390bd8cb2c1dda67f0e7127db89b010440
dlxla4820 <gorhfqhk@gmail.com>
2023-10-15 17:51:12 +0900

    SolvedRepository edit

--- src/main/java/kimdaehan/ctf/repository/SolvedRepository.java
+++ src/main/java/kimdaehan/ctf/repository/SolvedRepository.java
@@ -1,19 +1,16 @@
 package kimdaehan.ctf.repository;
 
+import kimdaehan.ctf.dto.UserPageDTO;
 import kimdaehan.ctf.entity.Quiz;
 import kimdaehan.ctf.entity.Solved;
 import kimdaehan.ctf.entity.SolvedId;
-import kimdaehan.ctf.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.lang.annotation.Native;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface SolvedRepository extends JpaRepository<Solved, SolvedId> {
@@ -25,10 +22,13 @@ public interface SolvedRepository extends JpaRepository<Solved, SolvedId> {
     List<Solved> findAllBySolved(Quiz quiz);
 
 
-    /*@Transactional(readOnly = true)
-    @Query(value = "SELECT solved_user_id, SUM(q.score) as total_score " +
-            "FROM solved s " +
-            "JOIN quiz q ON s.solved_quiz_id = q.quiz_id " +
-            "WHERE s.solved_user_id = :userId;", nativeQuery = true)
-    */
+    @Transactional(readOnly = true)
+    @Query(value = "SELECT CONVERT(u.user_id USING utf8) as user_id, u.name as name, u.affiliation as affiliation, CONVERT(u.nick_name USING utf8) as nick_name, u.registration_date_time as registration_date_time, " +
+            "       COALESCE(SUM(q.score), 0) AS total_score " +
+            "FROM user u " +
+            "LEFT JOIN solved s ON u.user_id = s.solved_user_id " +
+            "LEFT JOIN quiz q ON s.solved_quiz_id = q.quiz_id " +
+            "GROUP BY u.user_id, u.name, u.affiliation, u.nick_name, u.registration_date_time;", nativeQuery = true)
+    List<UserPageDTO> findScoreUsers();
+
 }
