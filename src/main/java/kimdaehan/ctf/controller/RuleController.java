b4b30a2725c02f1c71376f7f25eac0f1975d5769
dlxla4820 <gorhfqhk@gmail.com>
2023-10-31 08:45:45 +0900

    design edit

--- src/main/java/kimdaehan/ctf/controller/RuleController.java
+++ src/main/java/kimdaehan/ctf/controller/RuleController.java
@@ -2,7 +2,9 @@ package kimdaehan.ctf.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import kimdaehan.ctf.auth.AuthenticationFacade;
 import kimdaehan.ctf.entity.User;
+import kimdaehan.ctf.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class RuleController {
+public class RuleController extends BaseController{
+
+    public RuleController(UserService userService, AuthenticationFacade authenticationFacade) {
+        super(userService, authenticationFacade);
+    }
+
     @GetMapping({"/rule"})
     public ModelAndView scoreMain(HttpServletRequest request){
+        User user = getUser();
         ModelAndView mv = new ModelAndView("/rule");
         mv.addObject("title", "Rules");
-        HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("JSESSIONID");
         if(user == null){
             mv.addObject("user", null);
             mv.addObject("type", null);
